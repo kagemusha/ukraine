1a5bce136415f603df60b5c9a902e185c4bb8f07
kagemusha <mmadrid99@gmail.com>
2022-05-01 00:10:16 -0400

    formatting

--- app/components/hero/component.js
+++ app/components/hero/component.js
@@ -1,9 +1,9 @@
 import Component from '@glimmer/component';
 const peopleImgDir = '/assets/images/people';
 export default class HeroComponent extends Component {
-  hero = this.args.hero
+  hero = this.args.hero;
   imageCiv = `${peopleImgDir}/${this.hero.ic}`;
   imageMil = `${peopleImgDir}/${this.hero.im}`;
-  link = this.hero.link
-  hasLink = !!this.link
+  link = this.hero.link;
+  hasLink = !!this.link;
 }
--- app/routes/battlefields.js
+++ app/routes/battlefields.js
@@ -2,7 +2,7 @@ import Route from '@ember/routing/route';
 import battlefields from '../content/battlefields';
 
 export default class BattlefieldsRoute extends Route {
-    model() {
-        return battlefields;
-    }
+  model() {
+    return battlefields;
+  }
 }
