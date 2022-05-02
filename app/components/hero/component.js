50ca7a39af6dbb0711c8a4bbc9a6ae347c193f2c
kagemusha <mmadrid99@gmail.com>
2022-05-02 17:52:39 -0400

    remove obselete pics

--- app/components/hero/component.js
+++ app/components/hero/component.js
@@ -3,9 +3,8 @@ const peopleImgDir = '/assets/images/people';
 const imageBaseUrl = 'https://ik.imagekit.io/ubx4wnixel/uavictory/people';
 export default class HeroComponent extends Component {
   hero = this.args.hero;
-  // imageCiv = this.hero.iic ? `${imageBaseUrl}/${this.hero.iic}`  : `${peopleImgDir}/${this.hero.ic}`;
-  imageCiv = `${imageBaseUrl}/${this.hero.iic}`;  
-  imageMil = `${imageBaseUrl}/${this.hero.iim}`;
+  imageCiv = `${imageBaseUrl}/${this.hero.ic}`;  
+  imageMil = `${imageBaseUrl}/${this.hero.im}`;
   link = this.hero.link;
   hasLink = !!this.link;
 }
--- app/content/people.js
+++ app/content/people.js
@@ -7,20 +7,16 @@ const people = [
             to win.  Instead, Lomachenko returned to fight for Ukraine and is serving with.  After his
             service, he'll likely face the winner of Kambosis - Haney.
      `,
-    ic: 'loma-c.png',
-    im: 'loma-m.png',
-    iic: 'loma-c_wZpHZyrPl.png',
-    iim: 'loma-m_NnSDRmkpk.png',
+    ic: 'loma-c_wZpHZyrPl.png',
+    im: 'loma-m_NnSDRmkpk.png',
   },
   {
     n: 'Poro Poroshenko',
     d: `Despite facing treason charges and threat of arrest, former President Poro Poroshenko returned
             to Ukraine on the eve of the Russian invasion to help defend the country. Despite having to surrended
             his passport to authorities, he is currently commanding a battalion of the Territorial Defence Forces.`,
-    ic: 'poro-c.png',
-    im: 'poro-m.png',
-    iic: 'poro-c_a25gUhN9G.png',
-    iim: 'poro-m_RBOGKecyz.png',
+    ic: 'poro-c_a25gUhN9G.png',
+    im: 'poro-m_RBOGKecyz.png',
   },
   {
     n: 'Vitali and Wladimir Klitschko',
@@ -28,11 +24,8 @@ const people = [
             Wladimir joined the reserves as the threat of invasion was growing, and Vitaly, in his capacity as mayor,
             has been intimately involved in the defense of Kyiv.
           `,
-    ic: 'klit-c.png',
-    im: 'klit-m.png',
-    iic: 'klit-c_DuiUq4Ajj.png',
-    iim: 'klit-m_3nVN6MTVaP.png',
-    // link: '',
+    ic: 'klit-c_DuiUq4Ajj.png',
+    im: 'klit-m_3nVN6MTVaP.png',
   },
   {
     n: 'Anastasiia Lenna',
@@ -40,40 +33,30 @@ const people = [
             answer every beauty pageant's favorite question: how to secure world peace. In cases where
             your country is facing a brutal invasion, refer to pic on the right.
     `,
-    ic: 'lenn-c.png',
-    im: 'lenn-m.png',
-    iic: 'lenn-c_a3tAABAjb.png',
-    iim: 'lenn-m_33uBlYOBE.png',
-    // link: '',
+    ic: 'lenn-c_a3tAABAjb.png',
+    im: 'lenn-m_33uBlYOBE.png',
   },
   {
     n: 'Ivan Mishchenko',
     d: `Ivan Mishchenko is a Ukrainian Supreme Court judge.  A Supreme Court judge. Need we say more?`,
     // link: 'https://abovethelaw.com/2022/03/ukrainian-supreme-court-judge-earns-fierce-nickname-after-leaving-bench-for-battle-against-russia/',
-    ic: 'mish-c.png',
-    im: 'mish-m.png',
-    iic: 'mish-c_z7JLF8ohX.png',
-    iim: 'mish-m_lQBjBC1Y3.png',
+    ic: 'mish-c_z7JLF8ohX.png',
+    im: 'mish-m_lQBjBC1Y3.png',
   },
   {
     n: 'Kira Rudyk',
     d: `Rudyk was head of Ring Ukraine, before becoming a politician. She is currently a member the Ukrainian
         Parlaiment and head of the Voice party. She spends two hours a day on weapons training and has vowed to
         help defend her homeland.`,
-    ic: 'rudi-c.png',
-    im: 'rudi-m.png',
-    iic: 'rudi-c_5pEpTWAEc.png',
-    iim: 'rudi-m_gtBabHN14.png',
-    // link: '',
+    ic: 'rudi-c_5pEpTWAEc.png',
+    im: 'rudi-m_gtBabHN14.png',
   },
   {
     n: 'Maksim Kagal',
     d: `Maksim Kagal, a world kickboxing champion, joined the Azov Battalion and made the ultimate
             sacrific. He was killed defending Mariupol from Russian forces in March.`,
-    ic: 'kaga-c.png',
-    im: 'kaga-m.png',
-    iic: 'kaga-c_5B85bZCl9.png',
-    iim: 'kaga-m_MjT6aKYYm.png',
+    ic: 'kaga-c_5B85bZCl9.png',
+    im: 'kaga-m_MjT6aKYYm.png',
     // link: {t: 'Maksim Kagal killed', url: 'https://www.mirror.co.uk/sport/other-sports/kickboxing-world-champion-killed-ukraine-26579971'},
   },
   {
@@ -81,10 +64,8 @@ const people = [
     d: `Andriy Khlyvnyuk is front-man for popular Ukrainian band Boombox.  Along with his bandmates,
             he joined the Territorial Defense Forces where he was injured in a Russian mortar attack.  He has
             also recorded a version of the Ukrainian folksong Red Vibernum with none other than David Gilmour of Pink Floyd.`,
-    ic: 'khly-c.png',
-    im: 'khly-m.png',
-    iic: 'khly-c_2dlP6IYo9.png',
-    iim: 'khly-m_xMlSZdZPJ.png',
+    ic: 'khly-c_2dlP6IYo9.png',
+    im: 'khly-m_xMlSZdZPJ.png',
     link: {t: 'Hey Hey Rise Up with David Gilmour', url: 'https://www.youtube.com/watch?v=saEpkcVi1d4'},
   },
 ];
