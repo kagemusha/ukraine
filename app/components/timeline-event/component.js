1638c33c61b720918a13748768514f3fabd54250
kagemusha <mmadrid99@gmail.com>
2022-05-07 19:48:56 -0400

    lint

--- app/components/timeline-event/component.js
+++ app/components/timeline-event/component.js
@@ -6,5 +6,5 @@ export default class TimelineEventComponent extends Component {
   event = this.args.event;
   day1 = new Date('2022-02-24T00:00:00');
   date = this.event.d == 'Today' ? new Date() : new Date(this.event.d);
-  day = Math.ceil((this.date - this.day1)/DAY) + 1;
+  day = Math.ceil((this.date - this.day1) / DAY) + 1;
 }
