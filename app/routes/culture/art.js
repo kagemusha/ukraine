d9c7d9bb792731274bc57dc1f6a5b654b62ac343
kagemusha <mmadrid99@gmail.com>
2022-05-09 21:15:47 -0400

    add art images

--- app/routes/culture/art.js
+++ app/routes/culture/art.js
@@ -1,3 +1,8 @@
 import Route from '@ember/routing/route';
+import images from '../../content/art';
 
-export default class CultureArtRoute extends Route {}
+export default class CultureArtRoute extends Route {
+  model() {
+    return images;
+  }
+}
