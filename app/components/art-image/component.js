298ef15a79fd47d953c92651f675e0c8b9ee8283
kagemusha <mmadrid99@gmail.com>
2022-05-12 14:45:17 -0400

    lint

--- app/components/art-image/component.js
+++ app/components/art-image/component.js
@@ -3,6 +3,6 @@ import Component from '@glimmer/component';
 const ART_DIR = 'art';
 
 export default class ArtImageComponent extends Component {
-    image = this.args.image;
-    imageUrl = `${ART_DIR}/${this.image.url}`;
+  image = this.args.image;
+  imageUrl = `${ART_DIR}/${this.image.url}`;
 }
