3a336feba0a2020554908aadf104c5e52c8030a8
kagemusha <mmadrid99@gmail.com>
2022-05-12 10:45:29 -0400

    refactor latest news items to content dir

---
+++ app/content/latest-news.js
@@ -0,0 +1,46 @@
+/*
+  {
+    t: "", 
+    url: ''
+  },
+*/
+const items = [
+    {
+      t: "Poland issues stamp commemorating Zelensky's leadership",
+      url: 'https://twitter.com/ukraine_world/status/1524718178261258246',
+    },
+    {
+      t: 'May12th Russian loss estimates: 26,650 troops, 1195 tanks, 199 planes...',
+      url: 'https://twitter.com/KyivIndependent/status/1524663646009372673',
+    },
+    {
+      t: 'Lend-lease done; US House passes $40B aid package for Ukraine',
+      url: 'https://www.axios.com/2022/05/11/house-passes-40-billion-ukraine-bill',
+    },
+    {
+      t: 'Message from Ukraine to Poland: Dziękujemy',
+      url: 'https://twitter.com/visegrad24/status/1524304961202540545',
+    },
+    {
+      t: 'Latest battlefield assessment',
+      url: 'https://twitter.com/JominiW/status/1524228712325160960',
+    },
+    {
+      t: 'Dead Russian colonel count hits 40',
+      url: 'https://www.independent.co.uk/news/world/europe/putin-loses-39th-colonel-kharkiv-b2075368.html',
+    },
+    {
+      t: 'UA forces north of Kharkiv close in on Russian border',
+      url: 'https://twitter.com/War_Mapper/status/1523815508029714434',
+    },
+    {
+      t: 'May 9th: scaled-back parade. "Adverse weather" cancels flyover',
+      url: 'https://www.newsweek.com/russia-cancels-military-flyovers-victory-day-may-9-adverse-weather-1704656',
+    },
+    {
+      t: "Mariupol: Azovstal remains defiant",
+      url: 'https://twitter.com/mnogobitka/status/1524759610997059585',
+    },
+  ];
+
+  export default items;
