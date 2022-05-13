67f20b89db764ba73be1bd880f3b4a39be414b86
kagemusha <mmadrid99@gmail.com>
2022-05-13 08:50:37 -0400

    latest news

--- app/content/latest-news.js
+++ app/content/latest-news.js
@@ -5,46 +5,50 @@
   },
 */
 const items = [
-    {
-    t: "Nato to get reindeer; meatballs to follow (apologies to Finland and Sweden for our silly title :-)", 
-    url: 'https://bnn-news.com/finland-announces-intention-to-join-nato-234657'
-    },
-    {
-      t: "Poland issues stamp commemorating Zelensky's leadership",
-      url: 'https://twitter.com/ukraine_world/status/1524718178261258246',
-    },
-    {
-      t: 'May12th Russian loss estimates: 26,650 troops, 1195 tanks, 199 planes...',
-      url: 'https://twitter.com/KyivIndependent/status/1524663646009372673',
-    },
-    {
-      t: 'Lend-lease done; US House passes $40B aid package for Ukraine',
-      url: 'https://www.axios.com/2022/05/11/house-passes-40-billion-ukraine-bill',
-    },
-    {
-      t: 'Message from Ukraine to Poland: Dziękujemy',
-      url: 'https://twitter.com/visegrad24/status/1524304961202540545',
-    },
-    {
-      t: 'Latest battlefield assessment',
-      url: 'https://twitter.com/JominiW/status/1524228712325160960',
-    },
-    {
-      t: 'Dead Russian colonel count hits 40',
-      url: 'https://www.independent.co.uk/news/world/europe/putin-loses-39th-colonel-kharkiv-b2075368.html',
-    },
-    {
-      t: 'UA forces north of Kharkiv close in on Russian border',
-      url: 'https://twitter.com/War_Mapper/status/1523815508029714434',
-    },
-    {
-      t: 'May 9th: scaled-back parade. "Adverse weather" cancels flyover',
-      url: 'https://www.newsweek.com/russia-cancels-military-flyovers-victory-day-may-9-adverse-weather-1704656',
-    },
-    {
-      t: "Mariupol: Azovstal remains defiant",
-      url: 'https://twitter.com/mnogobitka/status/1524759610997059585',
-    },
-  ];
+  {
+    t: `Additional $521 in EU military aid to Ukraine`,
+    url: `https://twitter.com/KyivIndependent/status/1525045560390168579`,
+  },
+  {
+    t: `May 12 ISW Repport: UA Kharrkiv counterattack continues; Russian troubles at Izium`,
+    url: 'https://www.understandingwar.org/backgrounder/russian-offensive-campaign-assessment-may-12',
+  },
+  {
+    t: 'May13 Russian loss estimates: +250 troops, +10 tanks, 200th plane!...',
+    url: 'https://twitter.com/AlexKokcharov/status/1525048171726086144',
+  },
+  {
+    t: 'Nato to get reindeer; meatballs to follow (apologies to Finland and Sweden for our silly title :-)',
+    url: 'https://bnn-news.com/finland-announces-intention-to-join-nato-234657',
+  },
+  {
+    t: "Poland issues stamp commemorating Zelensky's leadership",
+    url: 'https://twitter.com/ukraine_world/status/1524718178261258246',
+  },
+  {
+    t: 'Lend-lease done; US House passes $40B aid package for Ukraine',
+    url: 'https://www.axios.com/2022/05/11/house-passes-40-billion-ukraine-bill',
+  },
+  {
+    t: 'Message from Ukraine to Poland: Dziękujemy',
+    url: 'https://twitter.com/visegrad24/status/1524304961202540545',
+  },
+  {
+    t: 'Dead Russian colonel count hits 40',
+    url: 'https://www.independent.co.uk/news/world/europe/putin-loses-39th-colonel-kharkiv-b2075368.html',
+  },
+  {
+    t: 'UA forces north of Kharkiv close in on Russian border',
+    url: 'https://twitter.com/War_Mapper/status/1523815508029714434',
+  },
+  {
+    t: 'May 9th: scaled-back parade. "Adverse weather" cancels flyover',
+    url: 'https://www.newsweek.com/russia-cancels-military-flyovers-victory-day-may-9-adverse-weather-1704656',
+  },
+  {
+    t: 'Mariupol: Azovstal remains defiant',
+    url: 'https://twitter.com/mnogobitka/status/1524759610997059585',
+  },
+];
 
-  export default items;
+export default items;
