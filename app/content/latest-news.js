b22d7650daab7f082237bdd5354a43662b17fe71
kagemusha <mmadrid99@gmail.com>
2022-05-17 07:15:06 -0400

    news

--- app/content/latest-news.js
+++ app/content/latest-news.js
@@ -2,7 +2,23 @@
  */
 const items = [
   {
-    t: `Who won Eurovision 2022? Hint: they also won the battles of Kyiv and Kharkiv`,
+    t: 'Zelensky tells Azovstal heroes to lay down arms to save lives. Prisoner swap expected',
+    url: 'https://twitter.com/HannaLiubakova/status/1526312579144028161',
+  },
+  {
+    t: 'May17 Russian loss estimates: +200 troops, +7 tanks, +345ATV...',
+    url: 'https://twitter.com/KyivIndependent/status/1526467383266795521',
+  },
+  {
+    t: `May 16 ISW Report: Russian try to hold some Kharkiv territory; stuck in the east `,
+    url: 'https://www.understandingwar.org/backgrounder/russian-offensive-campaign-assessment-may-16',
+  },
+  {
+    t: `UA troops reach the Russian border!`,
+    url: 'https://twitter.com/EuromaidanPress/status/1525968267424239616',
+  },
+  {
+    t: `Who won eurovision 2022? hint: they also won the battles of kyiv and kharkiv`,
     url: 'https://www.bbc.com/news/entertainment-arts-61452678',
   },
   {
@@ -10,24 +26,12 @@ const items = [
     url: 'https://www.cnbc.com/2022/05/14/russia-ukraine-live-updates.html',
   },
   {
-    t: 'May14 Russian loss estimates: +300 troops, +13 tanks, +34 ATV...',
-    url: 'https://twitter.com/KyivIndependent/status/1525390575368912897',
-  },
-  {
     t: `Svyatoslav Vakarchuk performed at the Chernobyl nuclear power plant`,
     url: 'https://euroweeklynews.com/2022/05/14/svyatoslav-vakarchuk-chernobyl-nuclear-power-plant/',
   },
   {
-    t: `Additional $521 in EU military aid to Ukraine`,
-    url: `https://twitter.com/KyivIndependent/status/1525045560390168579`,
-  },
-  {
     t: `Russians attempt to cross Siverskyi Donets River - get thumped`,
     url: `https://www.theguardian.com/world/2022/may/13/ukraine-has-repelled-russias-attempt-to-cross-donbas-river-uk-confirms`,
-  },
-  {
-    t: `May 12 ISW Repport: UA Kharrkiv counterattack continues; Russian troubles at Izium`,
-    url: 'https://www.understandingwar.org/backgrounder/russian-offensive-campaign-assessment-may-12',
   },
   {
     t: "Poland issues stamp commemorating Zelensky's leadership",
@@ -37,15 +41,15 @@ const items = [
     t: 'Message from Ukraine to Poland: Dziękujemy',
     url: 'https://twitter.com/visegrad24/status/1524304961202540545',
   },
-  {
-    t: 'Mariupol: Azovstal remains defiant',
-    url: 'https://twitter.com/mnogobitka/status/1524759610997059585',
-  },
 ];
 
 export default items;
 
 /* past
+  {
+    t: `Additional $521 in EU military aid to Ukraine`,
+    url: `https://twitter.com/KyivIndependent/status/1525045560390168579`,
+  },
 
   {
     t: `Israel - given the Lavrov kiss - approves Latvian transfer of Israeli weapons to Ukraine`,
