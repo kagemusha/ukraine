83183396d0a681c44f484212e10638a0874b3b35
kagemusha <mmadrid99@gmail.com>
2022-05-19 13:24:53 -0400

    news

--- app/content/latest-news.js
+++ app/content/latest-news.js
@@ -2,24 +2,28 @@
  */
 const items = [
   {
-    t: 'Sweden Signs NATO Request, Finland Formally Endorses Move',
-    url: 'https://www.nbcchicago.com/news/national-international/sweden-signs-nato-request-finns-formalize-intent-turkey-complicates-path/2834448/',
+    t: `UNSTOPPABLE: Kharkiv cat Stepan follows up Ukraine Eurovision triumph with Influencer Award win`,
+    url: 'https://twitter.com/KyivIndependent/status/1527236169251049473',
   },
   {
-    t: 'Zelensky tells Azovstal heroes to lay down arms to save lives. Prisoner swap expected',
-    url: 'https://twitter.com/HannaLiubakova/status/1526312579144028161',
+    t: `Liberation of Kharkiv Continues`,
+    url: 'https://twitter.com/KyivIndependent/status/1526988884026040320',
   },
   {
-    t: 'May18 Russian loss estimates: +400 troops, +16 tanks, +34 ATV...',
-    url: 'https://twitter.com/KyivIndependent/status/1526826546853564416?s=20&t=xlPvSVo4aeYLC3hgTaUNvg',
+    t: `Nato: Erdogan says will block Finland/Sweden, US expects resolution`,
+    url: 'https://www.reuters.com/world/europe/more-ukraine-fighters-surrendering-mariupol-russia-says-2022-05-18/',
+  },
+  {
+    t: 'International Red Cross Recording Names of Soldiers Surrendering at Azovstal', 
+    url: `https://www.pravda.com.ua/eng/news/2022/05/19/7347193/`,
+  },
+  {
+    t: 'May19 Russian loss estimates: +200 troops, +3 tanks, +20 APV, +14 drones...',
+    url: 'https://twitter.com/KyivIndependent/status/1527200530832949249',
   },
   {
     t: `May 18 ISW Report: Russians trying to hold north of Kharkiv, miniscule gains in East `,
     url: 'https://www.understandingwar.org/backgrounder/russian-offensive-campaign-assessment-may-18',
-  },
-  {
-    t: `UA troops reach the Russian border!`,
-    url: 'https://twitter.com/EuromaidanPress/status/1525968267424239616',
   },
   {
     t: `Who won eurovision 2022? hint: they also won the battles of kyiv and kharkiv`,
@@ -34,22 +38,22 @@ const items = [
     url: 'https://euroweeklynews.com/2022/05/14/svyatoslav-vakarchuk-chernobyl-nuclear-power-plant/',
   },
   {
-    t: `Russians attempt to cross Siverskyi Donets River - get thumped`,
-    url: `https://www.theguardian.com/world/2022/may/13/ukraine-has-repelled-russias-attempt-to-cross-donbas-river-uk-confirms`,
-  },
-  {
     t: "Poland issues stamp commemorating Zelensky's leadership",
     url: 'https://twitter.com/ukraine_world/status/1524718178261258246',
-  },
-  {
-    t: 'Message from Ukraine to Poland: Dziękujemy',
-    url: 'https://twitter.com/visegrad24/status/1524304961202540545',
   },
 ];
 
 export default items;
 
 /* past
+  {
+    t: 'Sweden Signs NATO Request, Finland Formally Endorses Move',
+    url: 'https://www.nbcchicago.com/news/national-international/sweden-signs-nato-request-finns-formalize-intent-turkey-complicates-path/2834448/',
+  },
+  {
+    t: 'Zelensky tells Azovstal heroes to lay down arms to save lives. Prisoner swap expected',
+    url: 'https://twitter.com/HannaLiubakova/status/1526312579144028161',
+  },
   {
     t: `Additional $521 in EU military aid to Ukraine`,
     url: `https://twitter.com/KyivIndependent/status/1525045560390168579`,
